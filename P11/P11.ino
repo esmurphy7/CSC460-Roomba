e704a1dc5562d383abbdc69e04079bf25a1b492d
Denis Kisselev <denis@dkisselev.net>
2016-01-20 17:53:30 -0800

    Add button + laser skeletons

--- P11/P11.ino
+++ P11/P11.ino
@@ -2,9 +2,11 @@
 
 /****** Pin Definitions *******/
 #define JOYSTICK_PIN 11
+#define BUTTON_PIN 21
 #define LIGHT_SENSOR_PIN 10
 
 #define SERVO_PIN 22
+#define LASER_PIN 23
 
 /****** Handles *******/
 Servo servo;
@@ -12,6 +14,7 @@ Servo servo;
 /****** Global State Variables *****/
 int joystickRead = 0;
 int lightRead = 0;
+int buttonState = 0; // 1 when clicked
 
 void setup() {
  
@@ -29,9 +32,18 @@ void readLight()
   
 }
 
+void readButton()
+{
+  
+}
 
 /****** Write Tasks *******/
 void writeServo()
+{
+  
+}
+
+void writeLaser()
 {
   
 }
