339b370f4acd2915420fd9f902053e5b2ca5a6c5
Denis Kisselev <denis@dkisselev.net>
2016-03-08 12:39:17 -0800

    b

--- src/main.c
+++ src/main.c
@@ -7,13 +7,18 @@
  *
  */
 #include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include "os.h"
 
 /*============
   * A Simple Test
   *============
   */
+
+enum { A=1, B, C, D, E, F, G };
+
+const unsigned int PT = 6;
+const unsigned char PPP[12] = {A, 5, B, 5, C, 5, D, 5, E, 5, F, 5};
 
 /**
   * A cooperative "Ping" task.
@@ -24,7 +29,7 @@ void Ping()
     int x;
 
     for(;;) {
-        PORTL = 0b00000010;
+        PORTL = 0xFF;
         _delay_ms(1000);
         Task_Next();
     }
@@ -40,25 +45,23 @@ void Pong()
     int x;
 
     for(;;) {
-        PORTL = 0b00000001;
+        PORTL = 0x00;
         _delay_ms(1000);
         Task_Next();
     }
 }
 
+EVENT* e;
 
 /**
   * This function creates two cooperative tasks, "Ping" and "Pong". Both
   * will run forever.
   */
-void main()
+int main(void)
 {
     DDRL = 0b11111111;
-    PORTL = 0xFF;
+    PORTL = 0x00;
 
-    OS_Init();
-    Task_Create(Pong, 0, 0, 0);
-    Task_Create(Ping, 0, 0, 0);
-
-    OS_Start();
+    Task_Create(Ping, 0, RR, 0);
+    Task_Create(Pong, 0, RR, 1);
 }
